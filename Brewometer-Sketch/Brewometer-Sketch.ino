cf60409035dff74d6161a5490e4f4f1d2f0edba0
Tyler Bryant <tyler.bryant@gmail.com>
2016-02-20 17:55:59 -0800

    calibration app + new sketch

--- Brewometer-Sketch/Brewometer-Sketch.ino
+++ Brewometer-Sketch/Brewometer-Sketch.ino
@@ -5,13 +5,14 @@ int counter = 0;
 ScratchData inputScratchData;
 
 uint8_t local_name[20];
-uint8_t ibeacon_uuid_swap[2];
+uint16_t ibeacon_uuid;
 
 int i;
 
 uint8_t tempOffset = 10;
 uint8_t sgOffset = 20;
-int16_t factoryTempOffset = 0;
+float factoryTempOffset = 0;
+float cal[4];
 double avgPitchPrev;
 
 const uint8_t ledScratch = 1;
@@ -25,13 +26,9 @@ int16_t getAvgTemperature(int count) {
   for (int i = 0; i < count; i++) {
     temperatureBuffer += Bean.getTemperature();
   }
-  float temperatureFahrenheit = (((float)temperatureBuffer / count) * 1.8) + 32 + tempOffset - 10;
+  float temperatureFahrenheit = (((float)temperatureBuffer / count) * 1.8) + 32 + factoryTempOffset + (tempOffset - 10);
 
   return int16_t(temperatureFahrenheit);
-}
-
-float cal(int index) {
-  return ((float *)local_name)[index + 1];
 }
 
 double getAvgPitch(int count) {
@@ -51,7 +48,7 @@ double getAvgPitch(int count) {
 }
 
 double convertPitch(double avgPitch) {
-  double sG  = cal(0) * avgPitch * avgPitch * avgPitch + cal(1) * avgPitch * avgPitch + cal(2) * avgPitch + cal(3) + sgOffset - 20;
+  double sG  = cal[0] * avgPitch * avgPitch * avgPitch + cal[1] * avgPitch * avgPitch + cal[2] * avgPitch + cal[3] + sgOffset - 20;
   return sG;
 }
 
@@ -63,8 +60,25 @@ void setup() {
   for ( i = 0 ; i < 20 ; i++) {
     local_name[i] = radioConfig.local_name[i];
   }
-  ibeacon_uuid_swap[0] = ((uint8_t *)&radioConfig.ibeacon_uuid)[1];
-  ibeacon_uuid_swap[1] = ((uint8_t *)&radioConfig.ibeacon_uuid)[0];
+  local_name[0] = 'B';
+  local_name[1] = 'r';
+  local_name[2] = 'e';
+  local_name[3] = 'w';
+  //multiply by appropriate factor per cal coeff
+  cal[0] = -0.000001 * (float)(((uint16_t *)(&(local_name[4])))[0]);
+  cal[1] = 0.0001 * (float)(((uint16_t *)(&(local_name[4])))[1]);
+  cal[2] = -0.001 * (float)(((uint16_t *)(&(local_name[4])))[2]);
+  cal[3] = 0.1 * (float)(((uint16_t *)(&(local_name[4])))[3]);
+  //temperature offset by 10
+  factoryTempOffset = (0.1 * ((uint16_t *)(&(local_name[4])))[4]) - 10;
+
+  ibeacon_uuid = radioConfig.ibeacon_uuid;
+
+  //reset user calibration
+  uint8_t userCal[2];
+  userCal[0] = tempOffset;
+  userCal[1] = sgOffset;
+  Bean.setScratchData(ledScratch, userCal, 2);
 }
 
 void resetRadioConfig() {
@@ -72,14 +86,13 @@ void resetRadioConfig() {
   radioConfig.conn_int = 20;
   radioConfig.power = 3;
   radioConfig.adv_mode = 1;
-  ((uint8_t *)&radioConfig.ibeacon_uuid)[0] = ibeacon_uuid_swap[1];
-  ((uint8_t *)&radioConfig.ibeacon_uuid)[1] = ibeacon_uuid_swap[0];
+  radioConfig.ibeacon_uuid = ibeacon_uuid;
   radioConfig.ibeacon_major = 0;
   radioConfig.ibeacon_minor = 0;
   for ( i = 0 ; i < 20 ; i++) {
     radioConfig.local_name[i] = local_name[i];
   }
-  radioConfig.local_name_size = 4;
+  radioConfig.local_name_size = 20;
 }
 
 void updateBeaconParameters(uint16_t major, uint16_t minor) {
@@ -119,6 +132,7 @@ void loop()
 
     avgPitchPrev = 0;
     Bean.setLed(0, 255, 0);
+    Bean.setLed(0, 0, 0);
 
     //look for command in Scratch 4
     bool newCommand = false;
@@ -130,28 +144,39 @@ void loop()
 
     switch (command) {
       case 1:
-        //Read
-        Bean.setScratchData(inputOutputScratch, local_name, 20);
+        //Read Coeffs
+        Bean.setScratchData(inputOutputScratch, &(local_name[4]), 10);
         endCommand();
         break;
       case 2:
-        //Write
+        //Write Coeffs
         inputScratchData = Bean.readScratchData(inputOutputScratch);
-        for (int i = 0; i < 20 ; i++) {
-          local_name[i] = inputScratchData.data[i];
+        for (int i = 0; i < 16 ; i++) {
+          local_name[i+4] = inputScratchData.data[i];
         }
+        //multiply by appropriate factor per cal coeff
+        cal[0] = -0.000001 * (float)(((uint16_t *)(&(local_name[4])))[0]);
+        cal[1] = 0.0001 * (float)(((uint16_t *)(&(local_name[4])))[1]);
+        cal[2] = -0.001 * (float)(((uint16_t *)(&(local_name[4])))[2]);
+        cal[3] = 0.1 * (float)(((uint16_t *)(&(local_name[4])))[3]);
+        //temperature offset by 10
+        factoryTempOffset = (0.1 * ((uint16_t *)(&(local_name[4])))[4]) - 10;
+        
         endCommand();
         break;
       case 3:
         //Read
-        Bean.setScratchData(inputOutputScratch, ibeacon_uuid_swap, 2);
+        //swap endian
+        uint8_t uuid_swapped[2];
+        uuid_swapped[0] = ((uint8_t *)&ibeacon_uuid)[1];
+        uuid_swapped[1] = ((uint8_t *)&ibeacon_uuid)[0];
+        Bean.setScratchData(inputOutputScratch, uuid_swapped, 2);
         endCommand();
         break;
       case 4:
         //Write
         inputScratchData = Bean.readScratchData(inputOutputScratch);
-        ibeacon_uuid_swap[0] = inputScratchData.data[0];
-        ibeacon_uuid_swap[1] = inputScratchData.data[1];
+        ibeacon_uuid = ((uint16_t *)inputScratchData.data)[0];
         endCommand();
         break;
       case 5:
@@ -161,10 +186,29 @@ void loop()
         endCommand();
         break;
       case 6:
+        //Measure Angle
+        uint8_t angleByteArray[4];
+        float angle;
+        angle = (float)getAvgPitch(16);
+        for(i = 0; i<4 ; i++){
+          angleByteArray[i] = ((uint8_t *)&angle)[i];
+        }
+        Bean.setScratchData(inputOutputScratch, angleByteArray, 4);
+        endCommand();
+        break;
+      case 7:
+        //Measure Temp
+        uint8_t tempByteArray[4];
+        float temp;
+        temp = (float)getAvgTemperature(8);
+        for(i = 0; i<4 ; i++){
+          tempByteArray[i] = ((uint8_t *)&temp)[i];
+        }
+        Bean.setScratchData(inputOutputScratch, tempByteArray, 4);
         endCommand();
         break;
       default:
-        //Bean.sleep(500);
+        Bean.sleep(500);
         break;
     }
   }
--- www/baronbrew.js
+++ www/baronbrew.js
@@ -12,7 +12,7 @@ baronbrew = function() {
 
     baronbrew.deselectBrewometer = function() {
         console.log("deselecting brewometer");
-        baronbrew.selectedBrewometer = ko.observable();
+        baronbrew.selectedBrewometer(null);
         baronbrew.disconnect();
         baronbrew.scan();
     }
@@ -25,54 +25,215 @@ baronbrew = function() {
     var UUID_SCRATCHSERVICE = 'a495ff20-c5b1-4b44-b512-1370f02d74de'.toUpperCase();
     getScratchCharacteristicUUID = function(scratchNumber) {
         return ['a495ff21-c5b1-4b44-b512-1370f02d74de'.toUpperCase(),
-        'a495ff22-c5b1-4b44-b512-1370f02d74de'.toUpperCase(),
-        'a495ff23-c5b1-4b44-b512-1370f02d74de'.toUpperCase(),
-        'a495ff24-c5b1-4b44-b512-1370f02d74de'.toUpperCase(),
-        'a495ff25-c5b1-4b44-b512-1370f02d74de'.toUpperCase()
+            'a495ff22-c5b1-4b44-b512-1370f02d74de'.toUpperCase(),
+            'a495ff23-c5b1-4b44-b512-1370f02d74de'.toUpperCase(),
+            'a495ff24-c5b1-4b44-b512-1370f02d74de'.toUpperCase(),
+            'a495ff25-c5b1-4b44-b512-1370f02d74de'.toUpperCase()
         ][scratchNumber - 1];
     };
+
+    var CalPoint = function(sg, angle) {
+        var self = this;
+        self.sg = ko.observable(sg);
+        self.angle = ko.observable(angle);
+    }
 
 
     var Brewometer = function(device) {
         var self = this;
         self.rssi = ko.observable(device.rssi);
         self.device = device;
-        self.color = ko.observable("0xBB10");
+        self.color = ko.observable();
+        self.command = 1;
+        self.selectedCalPointIndex = 0;
 
-        self.coeffs = ko.observableArray([1159 , 1341 , 6897 , 11977, 0]);
+        self.coeffs = ko.observableArray([]);
+        self.coeffsArray = new Uint16Array(5);
 
-        self.readUuid = function() {
-            var address = 1; //uuid
-            //set address in scratch 2
-            self.device.writeCharacteristic(getScratchCharacteristicUUID(2), address, function() {
-                console.log('wrote ' + getScratchCharacteristicUUID(2) + ' with address: ' + address);
-                var command = 1; //read
-                //set command to read
-                self.device.writeCharacteristic(getScratchCharacteristicUUID(1), command, function() {
-                }, function(e) {
-                    console.log('failed to write to ' + getScratchCharacteristicUUID(1) + ", " + e);
-                });
+        self.calPoints = ko.observableArray([new CalPoint(1000, 64.5), new CalPoint(1014, 60.5), new CalPoint(1051, 46.3), new CalPoint(1097, 22.5), new CalPoint(1127, 13.3)]);
+        self.name = ko.observable();
 
+        self.angle = ko.observable();
+        self.trueTemp = ko.observable(68);
+        self.measuredTemp = ko.observable(68);
 
+        self.computeCal = function() {
+
+            var data_x = [];
+            for (var i = 0; i < self.calPoints().length ; i++) {
+                data_x[i] = self.calPoints()[i].angle();
+            }
+
+            var data_y = [];
+            for (var i = 0; i < self.calPoints().length ; i++) {
+                data_y[i] = self.calPoints()[i].sg();
+            }
+            console.log(data_x[0]);
+            console.log(data_y[0]);
+
+            var cubic = function(params, x) {
+                return params[0] * x * x * x +
+                    params[1] * x * x +
+                    params[2] * x +
+                    params[3];
+            };
+
+            var objective = function(params) {
+                var total = 0.0;
+                for (var i = 0; i < data_x.length; ++i) {
+                    var resultThisDatum = cubic(params, data_x[i]);
+                    var delta = resultThisDatum - data_y[i];
+                    total += (delta * delta);
+                }
+                return total;
+            };
+
+            var initial = [1, 1, 1, 1];
+            var minimiser = numeric.uncmin(objective, initial);
+
+            console.log("initial:");
+            for (var j = 0; j < initial.length; ++j) {
+                console.log(initial[j]);
+            }
+
+            console.log("minimiser:");
+            for (var j = 0; j < minimiser.solution.length; ++j) {
+                console.log(minimiser.solution[j]);
+            }
+            coeffsArray = new Uint16Array(5)
+                //multiply and offset
+            coeffsArray[0] = minimiser.solution[0] * (-1000000);
+            coeffsArray[1] = minimiser.solution[1] * (10000);
+            coeffsArray[2] = minimiser.solution[2] * (-1000);
+            coeffsArray[3] = minimiser.solution[3] * (10);
+            coeffsArray[4] = (self.trueTemp() - self.measuredTemp() + 10) * 10;
+            self.coeffs(coeffsArray);
+
+        }
+
+        self.readCoeffs = function() {
+            self.command = 1;
+            var commandArray = new Uint8Array([self.command]); //read calibration
+            console.log('reading coeffs');
+
+            self.device.writeCharacteristic(getScratchCharacteristicUUID(4), commandArray, function() {
+                console.log('wrote ' + getScratchCharacteristicUUID(4) + ' with command: ' + self.command);
             }, function(e) {
-                console.log('failed to write to ' + getScratchCharacteristicUUID(2) + ", " + e);
-            })
+                console.log('failed to write to ' + getScratchCharacteristicUUID(4) + ", " + e);
+            });
+        }
+
+        self.writeCoeffs = function() {
+            self.command = 2;
+            var commandArray = new Uint8Array([self.command]); //write
+            console.log("writing coeffs " + hexStringFromUint8Array(self.coeffs()));
+
+            self.device.writeCharacteristic(getScratchCharacteristicUUID(5), self.coeffs(), function() {
+                self.device.writeCharacteristic(getScratchCharacteristicUUID(4), commandArray, function() {
+                    console.log('wrote ' + getScratchCharacteristicUUID(4) + ' with command: ' + self.command);
+                }, function(e) {
+                    console.log('failed to write to ' + getScratchCharacteristicUUID(4) + ", " + e);
+                });
+            }, function(e) {
+                console.log('failed to write to ' + getScratchCharacteristicUUID(5) + ", " + e);
+            });
 
         }
 
-        self.readCoeffs = function(){
+        self.readColor = function() {
+            self.command = 3;
+            var commandArray = new Uint8Array([self.command]); //read color
+            console.log('reading color');
 
+            self.device.writeCharacteristic(getScratchCharacteristicUUID(4), commandArray, function() {
+                console.log('wrote ' + getScratchCharacteristicUUID(4) + ' with command: ' + self.command);
+            }, function(e) {
+                console.log('failed to write to ' + getScratchCharacteristicUUID(4) + ", " + e);
+            });
         }
 
-        self.writeCoeffs = function(){
+        self.writeColor = function() {
+            self.command = 4;
+            var commandArray = new Uint8Array([self.command]); //write color
 
+            var colorArray = new Uint16Array([parseInt("0x" + self.color())]);
+            console.log('writing color, colorArray[0] = ' + colorArray[0]);
+            self.device.writeCharacteristic(getScratchCharacteristicUUID(5), colorArray, function() {
+                console.log('wrote ' + getScratchCharacteristicUUID(5) + ' with color: ' + self.color());
+                self.device.writeCharacteristic(getScratchCharacteristicUUID(4), commandArray, function() {
+                    console.log('wrote ' + getScratchCharacteristicUUID(4) + ' with command: ' + self.command);
+                }, function(e) {
+                    console.log('failed to write to ' + getScratchCharacteristicUUID(4) + ", " + e);
+                });
+            }, function(e) {
+                console.log('failed to write to ' + getScratchCharacteristicUUID(5) + ", " + e);
+            });
         }
 
-        self.readColor = function(){
+        self.readAngle = function() {
+            self.command = 6;
+            var commandArray = new Uint8Array([self.command]); //read pitch
+            console.log('reading angle');
 
+            self.device.writeCharacteristic(getScratchCharacteristicUUID(4), commandArray, function() {
+                console.log('wrote ' + getScratchCharacteristicUUID(4) + ' with command: ' + self.command);
+            }, function(e) {
+                console.log('failed to write to ' + getScratchCharacteristicUUID(4) + ", " + e);
+            });
         }
 
-        self.writeColor = function(){
+        self.readTemp = function() {
+            self.command = 7;
+            var commandArray = new Uint8Array([self.command]); //read temp
+            console.log('reading temp');
+
+            self.device.writeCharacteristic(getScratchCharacteristicUUID(4), commandArray, function() {
+                console.log('wrote ' + getScratchCharacteristicUUID(4) + ' with command: ' + self.command);
+            }, function(e) {
+                console.log('failed to write to ' + getScratchCharacteristicUUID(4) + ", " + e);
+            });
+        }
+
+
+        self.saveConfig = function() {
+            self.command = 5;
+            var commandArray = new Uint8Array([self.command]); //write config to persistent memory
+            console.log('writing config to persistent memory');
+            self.device.writeCharacteristic(getScratchCharacteristicUUID(4), commandArray, function() {
+                console.log('wrote ' + getScratchCharacteristicUUID(4) + ' with command: ' + self.command);
+            }, function(e) {
+                console.log('failed to write to ' + getScratchCharacteristicUUID(4) + ", " + e);
+            });
+        }
+
+
+        self.parseResponse = function(data) {
+            console.log('BLE characteristic data: ' + hexStringFromUint8Array(new Uint8Array(data)));
+            switch (self.command) {
+                case 1:
+                    //coeffs
+                    console.log("read coeffs " + hexStringFromUint8Array(data));
+                    self.coeffs(new Uint16Array(data));
+                    break;
+
+                case 3:
+                    //beaconId
+                    console.log("read beaconId " + hexStringFromUint8Array(new Uint8Array(data)));
+                    self.color(hexStringFromUint8Array(new Uint8Array(data)));
+                    break;
+
+                case 6:
+                    //angle
+                    console.log("reading angle " + hexStringFromUint8Array(new Uint8Array(data)));
+                    self.angle((new Float32Array(data))[0]);
+                    break;
+
+                case 7:
+                    //angle
+                    console.log("reading temp " + hexStringFromUint8Array(new Uint8Array(data)));
+                    self.measuredTemp((new Float32Array(data))[0]);
+                    break;
+            }
 
         }
 
@@ -82,16 +243,18 @@ baronbrew = function() {
 
             function onConnectSuccess(device) {
                 function onServiceSuccess(device) {
-                    console.log('enabling notifications')
-                    self.device.enableNotification(getScratchCharacteristicUUID(1),
+                    console.log('enabling notifications');
+                    self.device.enableNotification(
+                        getScratchCharacteristicUUID(5),
                         function(data) {
-                            console.log('notification ' + getScratchCharacteristicUUID(1));
-                            //self.parseCommandResponse(data);
-                            console.log(data);
+                            console.log('notification ' + getScratchCharacteristicUUID(5));
+                            self.parseResponse(data);
                         },
                         function() {
                             console.log('BLE startNotification error');
                         });
+
+                    self.readColor();
 
                 }
 
@@ -130,7 +293,7 @@ baronbrew = function() {
     hexStringFromUint8Array = function(data) {
         var hexString = '';
         for (var i = 0; i < data.length; i++) {
-            hexString += utils.toHexString(data[i], 1);
+            hexString += evothings.util.toHexString(data[i], 1);
         }
         return hexString;
     }
@@ -150,7 +313,7 @@ baronbrew = function() {
         evothings.easyble.startScan(deviceFound, onScanFailure);
     };
 
-    baronbrew.stopScan = function(){
+    baronbrew.stopScan = function() {
         evothings.easyble.stopScan();
     };
 
@@ -158,7 +321,7 @@ baronbrew = function() {
         evothings.easyble.closeConnectedDevices();
     };
 
-    baronbrew.cleanUp = function(){
+    baronbrew.cleanUp = function() {
         baronbrew.disconnect();
         baronbrew.stopScan();
 
